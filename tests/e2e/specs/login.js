d4185af3af0ae1ce5534b32ed78f0e940d7bfda9
nicolas-ortega <nicolasortega.y@gmail.com>
2021-09-01 19:56:05 -0400

    Initial commit

---
+++ tests/e2e/specs/login.js
@@ -0,0 +1,24 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe("login test suite", () => {
+
+  it("does not work with wrong credentials", () => {
+    cy.visit("/")
+
+    cy.get("[data-cy=username]").type("info")
+    cy.get("[data-cy=password]").type("visitor")
+    cy.get("[data-cy=login-btn]").click()
+
+    cy.location("pathname").should("equal", "/")
+  });
+
+  it('does work with valid credentials', () => {
+    cy.visit('/')
+
+    cy.get('[data-cy="username"]').type('test-e2e@boolean.cl')
+    cy.get('[data-cy="password"]').type('booleanacademia')
+    cy.get('[data-cy="login-btn"]').click()
+
+    cy.location('pathname').should('equal', '/productos')
+  });
+})
