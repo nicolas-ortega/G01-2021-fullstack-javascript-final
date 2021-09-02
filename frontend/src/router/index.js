6f647c862d5a7c53a91009d81b8235740007417c
nicolas-ortega <nicolasortega.y@gmail.com>
2021-09-01 22:56:34 -0400

    refactor(backend-firebase): Se agregó firebase-admin al backend para validar que las solicitudes estén autenticadas en las rutas /api

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
 import Products from '../views/Products.vue'
+import { Auth } from '@/firebase'
 
 Vue.use(VueRouter)
 
@@ -22,5 +23,17 @@ const router = new VueRouter({
   mode: 'history',
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const user = Auth.currentUser
+  const authRequired = to.matched.some(route => route.meta.login)
+
+  if (!user && authRequired) {
+    next('/')
+  } else {
+    next()
+  }
+})
+
 
 export default router
